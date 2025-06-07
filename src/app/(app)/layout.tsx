5ebadef9d9b84e59c18a08d1fff5ed0461641ad8
xdream oldlu <xdream@gmail.com>
2025-06-07 12:44:26 +0800

    01: init the project

---
+++ src/app/(app)/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next"
+import { Noto_Serif } from "next/font/google"
+
+import { ClerkProvider } from "@clerk/nextjs"
+
+import { Header } from "@/components/Header"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/AppSidebar"
+
+import "../globals.css"
+
+const notoSerif = Noto_Serif({
+	subsets: ["latin"],
+	variable: "--font-noto-serif",
+	weight: ["200", "500", "800"],
+})
+
+export const metadata: Metadata = {
+	title: "Reddit Clone",
+	description:
+		"Community for sharing and discussing your interests and assisted by AI",
+}
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode
+}>) {
+	return (
+		<ClerkProvider>
+			<html lang="en">
+				<body className={`${notoSerif.className} antialiased`}>
+					<SidebarProvider>
+						<AppSidebar />
+
+						<SidebarInset>
+							<Header />
+
+							<div className="flex flex-col">{children}</div>
+						</SidebarInset>
+					</SidebarProvider>
+				</body>
+			</html>
+		</ClerkProvider>
+	)
+}
---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+	return (
+		<div className="flex flex-col p-4">
+			<h1>hello world</h1>
+			<Button>hello again</Button>
+		</div>
+	)
+}
